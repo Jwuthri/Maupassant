ce6b6c16f7d1cc25f3b12b06c57729cd3c30ee1f
Wuthrich Julien <julien.wut@gmail.com>
2020-04-15 16:39:36 -0700

    update task file

--- maupassant/classifier/task.py
+++ maupassant/classifier/task.py
@@ -1,17 +1,36 @@
 import click
 
-from maupassant.classifier.train1 import TrainClassifier
+import pandas as pd
+
+from maupassant.classifier.train import Trainer
 from maupassant.classifier.predict import Predictor
 
 
 @click.command()
-@click.option("method", type=click.Choice(['train', 'predict']))
-@click.option("dataset_dir", type=str)
-@click.option("features", default="", type=list)
-@click.option("labels", default="", type=dict)
-def run(method, dataset_dir, features, labels):
+@click.argument("train_path", type=click.Path(exists=True))
+@click.argument("test_path", type=click.Path(exists=True))
+@click.argument("val_path", type=click.Path(exists=True))
+@click.argument("feature", type=str)
+@click.argument("label", type=str)
+@click.option("architecture", default="CNN_NN", type=str)
+@click.option("label_type", default="CNN_NN", type=str)
+def train(train_path, test_path, val_path, feature, label, architecture, label_type):
     """Run a specific task, as training or predicting."""
-    if method == "train":
-        trainer = TrainClassifier(dataset_dir, features, labels).fit_model()
-    else:
-        predictor = Predictor(dataset_dir)
+    train_df = pd.read_csv(train_path)
+    test_df = pd.read_csv(test_path)
+    val_df = pd.read_csv(val_path)
+    trainer = Trainer(train_df, test_df, val_df, label_type, architecture, feature, label)
+    model_path = trainer.main()
+
+    return model_path
+
+
+@click.command()
+@click.argument("model_path", type=click.Path(exists=True))
+@click.argument("example", type=str)
+def predict(model_path, example):
+    predictor = Predictor(model_path)
+    pred = predictor.predict_one(example)
+    print(pred)
+
+    return pred
