977322e2ab0267643b16f67476b9baa055e65eb7
julien Wuthrich <julien.wut@gmail.com>
2020-05-01 17:34:19 -0700

    update model

--- maupassant/summarizer/model.py
+++ maupassant/summarizer/model.py
@@ -1,13 +1,16 @@
 import os
+import math
 import pickle
+
+import numpy as np
+import pandas as pd
 
 import torch
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-from maupassant.preprocessing.normalization import TextNormalization
 from maupassant.preprocessing.tokenization import SequenceTokenization, SentenceTokenization
+from maupassant.preprocessing.normalization import TextNormalization
 from maupassant.settings import EXTERNAL_PATH
-
 from maupassant.utils import timer
 
 
@@ -34,9 +37,14 @@ class GoogleT5(object):
 
 class WeightedTfIdf(object):
 
-    def __init__(self):
+    def __init__(self, keywords):
         self.stemmer = TextNormalization()
         self.stopwords = pickle.load(open(os.path.join(EXTERNAL_PATH, 'stopwords.p'), "rb"))
+        self.keywords = keywords
+        self.augmented_keywords = self.keywords_augmentation()
+
+    def keywords_augmentation(self):
+        return self.keywords
 
     def create_dictionary(self, text):
         tokens = SentenceTokenization().tokenize(text)
@@ -44,6 +52,69 @@ class WeightedTfIdf(object):
 
         return dict((w, i) for i, w in enumerate(unique_words))
 
+    def create_matrix(self, dictionary, sentences):
+        words_count = len(dictionary)
+        sentences_count = len(sentences)
+        matrix = np.zeros((words_count, sentences_count))
+        sentence_to_token = {sentence: SentenceTokenization().tokenize(sentence) for sentence in sentences}
+        for col, sentence in enumerate(sentences):
+            for word in map(self.stemmer.word_stemming, sentence_to_token[sentence]):
+                if word in dictionary:
+                    row = dictionary[word]
+                    if word in self.keywords:
+                        matrix[row, col] += 3
+                    elif word in self.augmented_keywords:
+                        matrix[row, col] += 2.5
+                    else:
+                        matrix[row, col] += 1
+
+        return matrix
+
+    def compute_term_frequency(self, matrix, dictionary):
+        df = pd.DataFrame(matrix)
+        df["word"] = dictionary
+        ddf = df[~df["word"].isin(self.keywords)]
+        ddf = ddf.drop(columns=["word"])
+        ddf = ddf.as_matrix()
+
+        max_word_frequencies = np.max(ddf)
+        rows, cols = matrix.shape
+        for row in range(rows):
+            for col in range(cols):
+                max_word_frequency = max_word_frequencies
+                if max_word_frequency != 0:
+                    frequency = matrix[row, col] / max_word_frequency
+                    matrix[row, col] = frequency
+
+        return matrix
+
+    @staticmethod
+    def compute_ranks(sigma, v_matrix):
+        dimensions = max(3, int(len(sigma)))
+        powered_sigma = tuple(s ** 2 if i < dimensions else 0.0 for i, s in enumerate(sigma))
+
+        ranks = []
+        for column_vector in v_matrix.T:
+            rank = sum(s * v ** 2 for s, v in zip(powered_sigma, column_vector))
+            ranks.append(math.sqrt(rank))
+
+        return ranks
+
+    @staticmethod
+    def get_best_sentences(sentences, ranks):
+        vals = dict(zip(sentences, ranks))
+        res = {str(key): vals[key] for key in sorted(vals, key=vals.get, reverse=True)}
+
+        return res
+
     @timer
     def predict(self, text):
-        sentences = SequenceTokenization().tokenize(text)
+        sequence_to_sentences = SequenceTokenization().tokenize(text)
+        dictionary = self.create_dictionary(text)
+        tf = self.create_matrix(dictionary, sequence_to_sentences)
+        idf = self.compute_term_frequency(tf, dictionary)
+        u, sigma, v = np.linalg.svd(idf, full_matrices=False)
+        ranks = self.compute_ranks(sigma, v)
+        res = self.get_best_sentences(sequence_to_sentences, ranks)
+
+        return res
--- maupassant/summarizer/predict.py
+++ maupassant/summarizer/predict.py
@@ -2,40 +2,24 @@ import torch
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 from maupassant.utils import timer
+from maupassant.summarizer.model import WeightedTfIdf, GoogleT5
 
 
 class Predictor(object):
 
     def __init__(self, model_name='t5'):
         self.model_name = model_name
-        self.model = None
-        self.tokenizer = None
-        self.device = None
-        self.set_model()
+        self.model = self.set_model()
 
-    @timer
     def set_model(self):
         if self.model_name == "t5":
-            self.model = T5ForConditionalGeneration.from_pretrained('t5-small')
-            self.tokenizer = T5Tokenizer.from_pretrained('t5-small')
-            self.device = torch.device('cpu')
-
-    @timer
-    def summarize(self, tokenized_text):
-        return self.model.generate(
-            tokenized_text, num_beams=4, no_repeat_ngram_size=2,
-            min_length=30, max_length=100, early_stopping=True)
+            return GoogleT5()
+        else:
+            return WeightedTfIdf()
 
     @timer
     def predict(self, text):
-        processed_text = text
-        if self.model_name == "t5":
-            processed_text = "summarize: " + text.strip()
-
-        tokenized_text = self.tokenizer.encode(processed_text, return_tensors="pt").to(self.device)
-        relevant_indexes = self.summarize(tokenized_text)
-
-        return self.tokenizer.decode(relevant_indexes[0], skip_special_tokens=True)
+        return self.model.predict(text)
 
 
 if __name__ == '__main__':
